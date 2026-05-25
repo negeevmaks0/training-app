17d391a4f8e32246d41db124b7aca2a2b5bd4322
negeevmaks0 <negeevmaks@gmail.com>
2026-05-25 22:22:25 +0200

    Optimized code

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ class TrainingApp:
     def __init__(self):
         ui_settings = {
             "title": "Training",
+            'safe_area': False,
+            'padding': 0,
+            'spacing': 0,
         }
 
         self.ui = UI(ui_settings)
